7d0225557d8dce8864835deb5268e0dd95d6d9ec
jeovane lima <145929373+JeovaneSilva@users.noreply.github.com>
2024-04-23 00:26:46 -0300

    update tela de Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,57 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import LogoProjeto from '../../public/logoSyncEdit.png'
+import '../styles/global.css'
+import '../styles/Home.css'
+import { FaSearch, FaBars, FaEdit, FaTrash } from "react-icons/fa";
 
 const Home = () => {
   return (
-    <div>
-        <h1>Pagina de Home</h1>
-        <p>
-        <Link to="/Home/1">Texto 1</Link>
-        </p>
-        <p>
-        <Link to="/Home/2">Texto 2</Link>
-        </p>
-        <p>
-        <Link to="/Home/3">Texto 3</Link>
-        </p>
-    </div>
+    <>
+      <header>
+        <div className="logo">
+          <img src={LogoProjeto} alt="" />
+        </div>
+
+        <div className="BarraPesquisa">
+          <input type="search" name="BuscarUsers" id="BuscarUsers" placeholder='Buscar Usuários' />
+          <div className="IconLupa">
+            <FaSearch />
+          </div>
+        </div>
+
+        <div className="Menu">
+          <FaBars />
+        </div>
+      </header>
+
+     
+    <section>
+        <h1>Documentos de Texto</h1>
+        <div className="cardsProjects">
+
+            <div className="card">
+
+              <div className="infocard">
+                <h2>Teste</h2>
+                <p>Ultimo Acesso: 30/01/2024 </p>
+                <h3>Colaboradores</h3>
+                <span>0</span>
+              </div>
+
+              <div className='IconsCard'>
+                <div className="editarIcon">
+                  <FaEdit />
+                </div>
+                <div className="deletarIcon">
+                <FaTrash />
+                </div>
+              </div>
+
+            </div>
+
+        </div>     
+    </section>
+    </>
   )
 }
 
