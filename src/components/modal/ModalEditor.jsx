af450b20d78c65213cd2151c3c71a2d5c7313d3a
jeovane lima <145929373+JeovaneSilva@users.noreply.github.com>
2024-05-04 23:10:05 -0300

    update baixar documentos

--- src/components/modal/ModalEditor.jsx
+++ src/components/modal/ModalEditor.jsx
@@ -4,62 +4,98 @@ import JoditEditor from 'jodit-react';
 import { db } from '../../firebase/firebaseConfig';
 import ModalAddAmigoProject from './ModalAddAmigoProject';
 import { ModalEditorDiv,FooterEditor } from './stylesModais'
-import { CarregarProjetosProprios,CarregarProjetosColaborador } from '../../firebase/firebaseFunctions';
+import { CarregarProjetosProprios} from '../../firebase/firebaseFunctions';
+import html2pdf from 'html2pdf.js';
 
 const ModalEditor = ({content,uid,nomeProjeto,setModalEditor,setnewProjeto,nomesAmigos}) => {
 
     const editor = useRef(null)
     const ButonSalvar = useRef(null)
     const [modalAddAmigoProject, setmodalAddAmigoProject] = useState(false);
+    const [nomeEditado, setNomeEditado] = useState('');
 
     const config = useMemo(() => ({
         height: "75vh"
       }), []); // A configuração só será recriada se as dependências mudarem
 
-      const salvarContent = async () => {
-        try {
-          // Obtém o texto atual do projeto antes de atualizá-lo
-          const snapshot = await db.ref(`users/${uid}/documentos`).orderByChild('nameProject').equalTo(nomeProjeto).once('value');
-          snapshot.forEach((projetoSnapshot) => {
-            
-            // Atualiza o texto e o último acesso do projeto
-            projetoSnapshot.ref.update({
-              text: editor.current.value,
-            });
-            
-          });
-          alert("salvo")
+    const salvarContent = async () => {
+      try {
+        // Obtém o texto atual do projeto antes de atualizá-lo
+        const snapshot = await db.ref(`users/${uid}/documentos`).orderByChild('nameProject').equalTo(nomeProjeto).once('value');
+        snapshot.forEach((projetoSnapshot) => {
           
-        } catch (error) {
-          console.error("Erro ao salvar o texto do projeto:", error);
-        }
+          // Atualiza o texto e o último acesso do projeto
+          projetoSnapshot.ref.update({
+            text: editor.current.value,
+          });
+          
+        });
+        alert("salvo")
+        
+      } catch (error) {
+        console.error("Erro ao salvar o texto do projeto:", error);
+      }
+    };
+
+    const closeEditor = async () => {
+
+      const dataAtual = new Date();
+      const ano = dataAtual.getFullYear();
+      const mes = String(dataAtual.getMonth() + 1).padStart(2, '0');
+      const dia = String(dataAtual.getDate()).padStart(2, '0');
+      const dataFormatada = `${ano}-${mes}-${dia}`;
+    
+      try {
+        const snapshot = await db.ref(`users/${uid}/documentos`).orderByChild('nameProject').equalTo(nomeProjeto).once('value');
+        snapshot.forEach((projetoSnapshot) => {
+      
+          // Atualiza o último acesso do projeto
+          projetoSnapshot.ref.update({
+            ultimoAcesso: dataFormatada
+          });
+          
+        });
+        
+      } catch (error) {
+        console.error("Erro ao salvar o texto do projeto:", error);
+      }
+          setModalEditor(false);
+          CarregarProjetosProprios(uid, setnewProjeto);
+      }
+
+      const handleDownloadPDF = () => {
+        const contentHtml = editor.current.value; 
+    
+        const opt = {
+          margin: 1,
+          filename: `${nomeProjeto}.pdf`,
+          image: { type: 'jpeg', quality: 0.98 },
+          html2canvas: { scale: 2 },
+          jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
+        };
+    
+        html2pdf().set(opt).from(contentHtml).save();
       };
 
-      const closeEditor = async () => {
+      const handleNomeChange = (event) => {
+        setNomeEditado(event.target.value);
+      };
 
-        const dataAtual = new Date();
-        const ano = dataAtual.getFullYear();
-        const mes = String(dataAtual.getMonth() + 1).padStart(2, '0');
-        const dia = String(dataAtual.getDate()).padStart(2, '0');
-        const dataFormatada = `${ano}-${mes}-${dia}`;
-      
+      const MudarNomeProjeto = async () => {
         try {
           const snapshot = await db.ref(`users/${uid}/documentos`).orderByChild('nameProject').equalTo(nomeProjeto).once('value');
           snapshot.forEach((projetoSnapshot) => {
-        
-            // Atualiza o último acesso do projeto
             projetoSnapshot.ref.update({
-              ultimoAcesso: dataFormatada
+              nameProject: nomeEditado,
             });
             
           });
+          alert("nome modificado")
           
         } catch (error) {
           console.error("Erro ao salvar o texto do projeto:", error);
         }
-            setModalEditor(false);
-            CarregarProjetosProprios(uid, setnewProjeto);
-        }
+      }
 
   return (
     <>
@@ -73,8 +109,8 @@ const ModalEditor = ({content,uid,nomeProjeto,setModalEditor,setnewProjeto,nomes
             <div>
               <label>Editar Nome:</label>
               <div>
-                <input type="text" />
-                <FaEdit/>
+                <input type="text" onChange={handleNomeChange} placeholder={nomeProjeto} />
+                {nomeEditado && <FaEdit onClick={MudarNomeProjeto} />}
               </div>
             </div>
 
@@ -86,7 +122,7 @@ const ModalEditor = ({content,uid,nomeProjeto,setModalEditor,setnewProjeto,nomes
               </div> 
 
               <div>
-                <button>Baixar</button>
+                <button onClick={handleDownloadPDF}>Baixar</button>
                 <button onClick={() => setmodalAddAmigoProject(true)}>Convidar</button>
               </div>
 
--- src/components/modal/ModalEditorColab.jsx
+++ src/components/modal/ModalEditorColab.jsx
@@ -1,14 +1,17 @@
-import React, {useRef,useMemo} from 'react'
+import React, {useState,useRef,useMemo} from 'react'
 import {FaEdit} from "react-icons/fa";
 import JoditEditor from 'jodit-react';
 import { ModalEditorDiv,FooterEditor } from './stylesModais'
 import { CarregarProjetosColaborador } from '../../firebase/firebaseFunctions';
 import { db } from '../../firebase/firebaseConfig';
+import html2pdf from 'html2pdf.js';
 
 const ModalEditorColab = ({content,nomeProjeto,setModalEditorColaborador,uid,userName,setProjetosColaborador}) => {
 
     const editor = useRef(null)
     const ButonSalvar = useRef(null)
+
+    const [nomeEditado, setNomeEditado] = useState('');
 
     const config = useMemo(() => ({
         height: "75vh"
@@ -56,9 +59,7 @@ const ModalEditorColab = ({content,nomeProjeto,setModalEditorColaborador,uid,use
             Object.values(usersData).forEach(async (user) => {
               if (user.documentos) {
                 Object.entries(user.documentos).forEach(async ([key, projeto]) => {
-                  // Verifica se o documento corresponde ao projeto original com o mesmo nome do projeto atual
                   if (projeto.nameProject === nomeProjeto && !projeto.colaborador) {
-                    // Atualiza o texto do projeto original com o conteúdo digitado pelo colaborador
                     await db.ref(`users/${user.id}/documentos/${key}`).update({
                       ultimoAcesso: dataFormatada
                     })
@@ -76,6 +77,49 @@ const ModalEditorColab = ({content,nomeProjeto,setModalEditorColaborador,uid,use
           CarregarProjetosColaborador(uid, userName, setProjetosColaborador);
       };
 
+      const handleDownloadPDF = () => {
+        const contentHtml = editor.current.value; 
+    
+        const opt = {
+          margin: 1,
+          filename: `${nomeProjeto}.pdf`,
+          image: { type: 'jpeg', quality: 0.98 },
+          html2canvas: { scale: 2 },
+          jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
+        };
+    
+        html2pdf().set(opt).from(contentHtml).save();
+      };
+
+      const handleNomeChange = (event) => {
+        setNomeEditado(event.target.value);
+      };
+
+      const MudarNomeProjeto = async () => {
+        try {
+          const snapshot = await db.ref(`users`).once('value');
+          const usersData = snapshot.val();
+      
+          if (usersData) {
+            Object.values(usersData).forEach(async (user) => {
+              if (user.documentos) {
+                Object.entries(user.documentos).forEach(async ([key, projeto]) => {
+                  if (projeto.nameProject === nomeProjeto && !projeto.colaborador) {
+                    await db.ref(`users/${user.id}/documentos/${key}`).update({
+                      nameProject: nomeEditado
+                    })
+                  }
+                  
+                });
+              }
+            });
+          }
+          alert("nome editado")
+        } catch (error) {
+          console.error("Erro ao salvar o texto do projeto colaborador:", error);
+        }
+      }
+
   return (
     <ModalEditorDiv>
         <JoditEditor
@@ -87,8 +131,8 @@ const ModalEditorColab = ({content,nomeProjeto,setModalEditorColaborador,uid,use
             <div>
               <label>Editar Nome:</label>
               <div>
-                <input type="text" />
-                <FaEdit/>
+              <input type="text" onChange={handleNomeChange} placeholder={nomeProjeto} />
+              {nomeEditado && <FaEdit onClick={MudarNomeProjeto} />}
               </div>
             </div>
 
@@ -99,7 +143,7 @@ const ModalEditorColab = ({content,nomeProjeto,setModalEditorColaborador,uid,use
             </div>
 
             <div>
-              <button>Baixar</button>
+              <button onClick={handleDownloadPDF}>Baixar</button>
               <button>Membros</button>
             </div>
           </div>
