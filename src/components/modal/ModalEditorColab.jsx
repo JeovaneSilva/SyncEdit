508703236f9cdfa9716d6796ed734f9cd5c9f179
jeovane lima <145929373+JeovaneSilva@users.noreply.github.com>
2024-05-06 19:07:27 -0300

    teste

--- src/components/modal/ModalEditorColab.jsx
+++ src/components/modal/ModalEditorColab.jsx
@@ -1,7 +1,6 @@
-import React, {useState,useRef,useMemo, useEffect} from 'react'
-import {FaEdit, FaSyncAlt} from "react-icons/fa";
-// import JoditEditor from 'jodit-react';
-import { ModalEditorDiv,FooterEditor } from './stylesModais'
+import React, { useState, useEffect } from 'react';
+import { FaSyncAlt } from "react-icons/fa";
+import { ModalEditorDiv, FooterEditor } from './stylesModais';
 import { CarregarProjetosColaborador } from '../../firebase/firebaseFunctions';
 import { db } from '../../firebase/firebaseConfig';
 import html2pdf from 'html2pdf.js';
@@ -9,287 +8,143 @@ import ModalMostrarMenbros from './ModalMostrarMenbros';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
-const ModalEditorColab = ({setContent,content,nomeProjeto,setModalEditorColaborador,uid,userName,setProjetosColaborador}) => {
+const ModalEditorColab = ({ content, nomeProjeto, setModalEditorColaborador, uid, userName, setProjetosColaborador }) => {
+    const [modalMenbros, setModalMenbros] = useState(false);
 
-  const editor = useRef(null)
-  const ButonSalvar = useRef(null)
+    const closeEditorColaborador = async () => {
+        const dataAtual = new Date();
+        const ano = dataAtual.getFullYear();
+        const mes = String(dataAtual.getMonth() + 1).padStart(2, '0');
+        const dia = String(dataAtual.getDate()).padStart(2, '0');
+        const dataFormatada = `${ano}-${mes}-${dia}`;
 
-  const [nomeEditado, setNomeEditado] = useState('');
-  const [modalMenbros, setModalMenbros] = useState(false)
-
-  // const config = useMemo(() => ({
-  //     height: "75vh",
-  //     placeholder: '',
-  //     autofocus: true,
-  //     readonly: false,
-	// 	  cursorAfterAutofocus: 'end'
-  //   }), []); 
-
-  const SalvarContentColab = async () => {
-    try {
-      const snapshot = await db.ref(`users`).once('value');
-      const usersData = snapshot.val();
-  
-      if (usersData) {
-        Object.values(usersData).forEach(async (user) => {
-          if (user.documentos) {
-            Object.entries(user.documentos).forEach(async ([key, projeto]) => {
-              // Verifica se o documento corresponde ao projeto original com o mesmo nome do projeto atual
-              if (projeto.nameProject === nomeProjeto && !projeto.colaborador) {
-                // Atualiza o texto do projeto original com o conteúdo digitado pelo colaborador
-                await db.ref(`users/${user.id}/documentos/${key}`).update({
-                  text: content
-                })
-              }
-              
-            });
-          }
-        });
-      }
-      alert("salvo")
-    } catch (error) {
-      console.error("Erro ao salvar o texto do projeto colaborador:", error);
-    }
-  }
-
-  const handleContentChange = async (newContent) => {
-    setContent(newContent);
-    try {
-        // Atraso de 1 segundo antes de salvar
-        setTimeout(async () => {
+        try {
             const snapshot = await db.ref(`users`).once('value');
             const usersData = snapshot.val();
+
             if (usersData) {
                 Object.values(usersData).forEach(async (user) => {
                     if (user.documentos) {
                         Object.entries(user.documentos).forEach(async ([key, projeto]) => {
                             if (projeto.nameProject === nomeProjeto && !projeto.colaborador) {
                                 await db.ref(`users/${user.id}/documentos/${key}`).update({
-                                    text: newContent
-                                });
+                                    ultimoAcesso: dataFormatada
+                                })
                             }
                         });
                     }
                 });
             }
-        }, 1000);
-    } catch (error) {
-        console.error("Erro ao salvar o texto do projeto colaborador:", error);
-    }
-};
-
-  const closeEditorColaborador = async () => {
-    const dataAtual = new Date();
-    const ano = dataAtual.getFullYear();
-    const mes = String(dataAtual.getMonth() + 1).padStart(2, '0');
-    const dia = String(dataAtual.getDate()).padStart(2, '0');
-    const dataFormatada = `${ano}-${mes}-${dia}`;
-  
-    try {
-      const snapshot = await db.ref(`users`).once('value');
-      const usersData = snapshot.val();
-  
-      if (usersData) {
-        Object.values(usersData).forEach(async (user) => {
-          if (user.documentos) {
-            Object.entries(user.documentos).forEach(async ([key, projeto]) => {
-              if (projeto.nameProject === nomeProjeto && !projeto.colaborador) {
-                await db.ref(`users/${user.id}/documentos/${key}`).update({
-                  ultimoAcesso: dataFormatada
-                })
-              }
-              
-            });
-          }
-        });
-      }
-    } catch (error) {
-      console.error("Erro ao salvar o texto do projeto colaborador:", error);
-    }
-  
-      setModalEditorColaborador(false);
-      CarregarProjetosColaborador(uid, userName, setProjetosColaborador);
-  };
-
-  const handleDownloadPDF = () => {
-    const contentHtml = editor.current.value; 
-
-    const opt = {
-      margin: 1,
-      filename: `${nomeProjeto}.pdf`,
-      image: { type: 'jpeg', quality: 0.98 },
-      html2canvas: { scale: 2 },
-      jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
-    };
-
-    html2pdf().set(opt).from(contentHtml).save();
-  };
-
-  const handleNomeChange = (event) => {
-    setNomeEditado(event.target.value);
-  };
-
-  const MudarNomeProjeto = async () => {
-    try {
-      const snapshot = await db.ref(`users`).once('value');
-      const usersData = snapshot.val();
-  
-      if (usersData) {
-        Object.values(usersData).forEach(async (user) => {
-          if (user.documentos) {
-            Object.entries(user.documentos).forEach(async ([key, projeto]) => {
-              if (projeto.nameProject === nomeProjeto && !projeto.colaborador) {
-                await db.ref(`users/${user.id}/documentos/${key}`).update({
-                  nameProject: nomeEditado
-                })
-              }
-              
-            });
-          }
-        });
-      }
-      alert("nome editado")
-    } catch (error) {
-      console.error("Erro ao salvar o texto do projeto colaborador:", error);
-    }
-  }
-
-  const MostrarMembros = () => {
-    setModalMenbros(true)
-  }
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const snapshot = await db.ref(`users`).once('value');
-        const usersData = snapshot.val();
-
-        if (usersData) {
-          Object.values(usersData).forEach((user) => {
-            if (user.documentos) {
-              Object.entries(user.documentos).forEach(([key, doc]) => {
-                if (doc.nameProject === nomeProjeto) {
-                  setContent(doc.text || '');
-                }
-              });
-            }
-          });
+        } catch (error) {
+            console.error("Erro ao salvar o texto do projeto colaborador:", error);
         }
-      } catch (error) {
-        console.error("Erro ao recuperar texto do projeto colaborador:", error);
-      }
+
+        setModalEditorColaborador(false);
+        CarregarProjetosColaborador(uid, userName, setProjetosColaborador);
     };
 
-    fetchData();
+    const handleDownloadPDF = () => {
+        const contentHtml = content;
 
-    // Adicionar listener de evento para atualizações em tempo real
-    const projectRef = db.ref(`users`);
-    projectRef.on('value', snapshot => {
-      const usersData = snapshot.val();
+        const opt = {
+            margin: 1,
+            filename: `${nomeProjeto}.pdf`,
+            image: { type: 'jpeg', quality: 0.98 },
+            html2canvas: { scale: 2 },
+            jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
+        };
 
-      if (usersData) {
-        Object.values(usersData).forEach((user) => {
-          if (user.documentos) {
-            Object.entries(user.documentos).forEach(([key, doc]) => {
-              if (doc.nameProject === nomeProjeto) {
-                setContent(doc.text || '');
-              }
-            });
-          }
+        html2pdf().set(opt).from(contentHtml).save();
+    };
+
+    const MostrarMembros = () => {
+        setModalMenbros(true)
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const snapshot = await db.ref(`users`).once('value');
+                const usersData = snapshot.val();
+
+                if (usersData) {
+                    Object.values(usersData).forEach((user) => {
+                        if (user.documentos) {
+                            Object.entries(user.documentos).forEach(([key, doc]) => {
+                                if (doc.nameProject === nomeProjeto) {
+                                    setContent(doc.text || '');
+                                }
+                            });
+                        }
+                    });
+                }
+            } catch (error) {
+                console.error("Erro ao recuperar texto do projeto colaborador:", error);
+            }
+        };
+
+        fetchData();
+
+        // Adicionar listener de evento para atualizações em tempo real
+        const projectRef = db.ref(`users`);
+        projectRef.on('value', snapshot => {
+            const usersData = snapshot.val();
+
+            if (usersData) {
+                Object.values(usersData).forEach((user) => {
+                    if (user.documentos) {
+                        Object.entries(user.documentos).forEach(([key, doc]) => {
+                            if (doc.nameProject === nomeProjeto) {
+                                setContent(doc.text || '');
+                            }
+                        });
+                    }
+                });
+            }
         });
-      }
-    });
 
-    return () => {
-      // Limpar o listener quando o componente é desmontado
-      projectRef.off('value');
-    };
-  }, [uid, nomeProjeto]);
+        return () => {
+            // Limpar o listener quando o componente é desmontado
+            projectRef.off('value');
+        };
+    }, [uid, nomeProjeto]);
 
-  const modules = {
-    toolbar: [
-      [{ 'header': '1' }, { 'header': '2' }, { 'font': [] }],
-      [{ 'size': [] }],
-      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-      [{ 'list': 'ordered' }, { 'list': 'bullet' },
-       { 'indent': '-1' }, { 'indent': '+1' }],
-      ['link', 'image', 'video'],
-      ['align', 'color', 'background'], // Adicionando alinhamento, cor do texto e cor de fundo
-      ['script', 'formula'], // Adicionando script e fórmula
-      ['code-block'], // Adicionando bloco de código
-      ['clean']
-    ]
-  };
+    return (
+        <>
+            <ModalEditorDiv>
+                <div style={{ backgroundColor: 'white', height: '75%', padding: '10px' }}>
+                    <ReactQuill
+                        value={content}
+                        modules={{ toolbar: false }}
+                        readOnly={true}
+                        theme="snow"
+                    />
+                </div>
+                <FooterEditor>
+                    <div>
+                        <label>Nome do Projeto: {nomeProjeto}</label>
+                    </div>
+                    <div>
+                        <div>
+                            <button onClick={closeEditorColaborador}>Fechar</button>
+                        </div>
+                        <div>
+                            <button onClick={handleDownloadPDF}>Baixar</button>
+                            <button onClick={MostrarMembros}>Membros</button>
+                        </div>
+                    </div>
+                </FooterEditor>
+            </ModalEditorDiv>
 
-  const formats = [
-    'header', 'font', 'size',
-    'bold', 'italic', 'underline', 'strike', 'blockquote',
-    'list', 'bullet', 'indent',
-    'link', 'image', 'video',
-    'align', 'color', 'background',
-    'script', 'formula',
-    'code-block'
-  ];
-
-  const editorStyle = {
-    backgroundColor: 'white',
-    height: '75%',
-    padding: '10px'
-  };
-
-  return (
-    <>
-    <ModalEditorDiv>
-    <div style={editorStyle}>
-      <ReactQuill
-        value={content}
-        onChange={handleContentChange}
-        modules={modules}
-        formats={formats}
-        theme="snow"
-      />
-    </div>
-
-        {/* <JoditEditor
-          ref={editor}
-          value={content}
-          config={config}
-          onBlur={handleContentChange}
-        /> */}
-        <FooterEditor>
-            <div>
-              <label>Editar Nome:</label>
-              <div>
-              <input type="text" onChange={handleNomeChange} placeholder={nomeProjeto} />
-              {nomeEditado && <FaEdit onClick={MudarNomeProjeto} />}
-              <FaSyncAlt />
-              </div>
-            </div>
-
-          <div>
-            <div>
-              <button ref={ButonSalvar} onClick={SalvarContentColab}>Salvar</button>
-              <button onClick={closeEditorColaborador}>Fechar</button>
-            </div>
-
-            <div>
-              <button onClick={handleDownloadPDF}>Baixar</button>
-              <button onClick={MostrarMembros}>Membros</button>
-            </div>
-          </div>
-        </FooterEditor>
-          
-      </ModalEditorDiv>
-
-      {modalMenbros &&
-        <ModalMostrarMenbros
-        uid={uid}
-        nomeProjeto={nomeProjeto}
-        setModalMenbros={setModalMenbros}
-        />
-       }
-      </>
-  )
+            {modalMenbros &&
+                <ModalMostrarMenbros
+                    uid={uid}
+                    nomeProjeto={nomeProjeto}
+                    setModalMenbros={setModalMenbros}
+                />
+            }
+        </>
+    )
 }
 
-export default ModalEditorColab
+export default ModalEditorColab;
